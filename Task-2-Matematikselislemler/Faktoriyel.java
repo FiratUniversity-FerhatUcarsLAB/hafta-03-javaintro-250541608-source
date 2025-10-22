2bf0e6018b273a6cc0fe078d4bb57cf7d614f085
halid hacbekkur <250541608@firat.edu.tr>
2025-10-22 19:48:42 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,8 @@
+// Faktoriyel.java
+public class Faktoriyel {
+    public static void main(String[] args) {
+        int sayi = 5;
+        int sonuc = 5 * 4 * 3 * 2 * 1;
+        System.out.println(sayi + "! = " + sonuc);
+    }
+}
