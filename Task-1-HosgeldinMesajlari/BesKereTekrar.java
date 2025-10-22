e92b3c36952ee5a19059db15243431882ac2b8c2
halid hacbekkur <250541608@firat.edu.tr>
2025-10-22 18:52:05 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,10 @@
+// BesKere.java
+public class BesKere {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+    }
+}
