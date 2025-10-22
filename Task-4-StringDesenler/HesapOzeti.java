78d47bcd307461a991e0cf922faf9b2ad57203dc
halid hacbekkur <250541608@firat.edu.tr>
2025-10-22 19:54:01 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,10 @@
+// HesapOzeti.java
+public class HesapOzeti {
+    public static void main(String[] args) {
+        System.out.println("Ürün\tMiktar\tFiyat");
+        System.out.println("---------------------------");
+        System.out.println("Ekmek\t2\t10 TL");
+        System.out.println("Süt\t1\t15 TL");
+        System.out.println("Yumurta\t1\t30 TL");
+    }
+}
