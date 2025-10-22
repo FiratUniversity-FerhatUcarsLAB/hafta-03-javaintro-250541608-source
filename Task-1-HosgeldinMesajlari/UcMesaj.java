1cff702cb199e60a830fc7c4e37d0ce6e9ff456f
halid hacbekkur <250541608@firat.edu.tr>
2025-10-22 19:48:14 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,18 @@
+
+        // UcMesaj.java
+        public class UcMesaj {
+            public static void main(String[] args) {
+                System.out.println("Java'ya Hoş Geldiniz");
+                System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+                System.out.println("Programlama Eğlencelidir");
+            }
+        }
+
+
+
+
+
+
+
+
+
