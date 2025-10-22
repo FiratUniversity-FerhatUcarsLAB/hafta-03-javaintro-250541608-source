fc8ab1a67e36d6376287c8891eb4fdc3b2b2b9de
halid hacbekkur <250541608@firat.edu.tr>
2025-10-22 19:54:34 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,18 @@
+// IsimKarti.java
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Halid";
+        String soyad = "Hacbekkur";
+        String ogrNo = "250541608";
+        String bolum = "Yazılım Mühendisliği";
+
+        System.out.println("====================");
+        System.out.println("   ÖĞRENCİ BİLGİLERİ   ");
+        System.out.println("====================");
+        System.out.println("Ad: " + ad);
+        System.out.println("Soyad: " + soyad);
+        System.out.println("Numara: " + ogrNo);
+        System.out.println("Bölüm: " + bolum);
+        System.out.println("====================");
+    }
+}
